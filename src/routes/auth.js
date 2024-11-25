db4034627de31013e9440eb28186fc01dfe33014
walidtoni <walidsyadansultoni@gmail.com>
2024-11-25 12:54:31 +0700

    change functions & queries to prisma based

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,8 @@
+// import express from 'express';
 const express = require('express');
+const prisma = require('../db/prisma');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const db = require('../config/db');
 require('dotenv').config();
 
 const router = express.Router();
@@ -16,48 +17,65 @@ router.post('/register', async (req, res) => {
   }
 
   // Cek apakah email sudah terdaftar
-  db.query('SELECT * FROM users WHERE email = ?', [email], async (err, result) => {
-    if (err) return res.status(500).json({ message: 'Database error.' });
-    if (result.length > 0) {
-      return res.status(400).json({ message: 'Email already registered.' });
+  const existedEmail = await prisma.user.findFirst({
+    where: {
+      email
     }
+  })
 
-    // Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
+  // if (err) return res.status(500).json({ message: 'Database error.' });
 
-    // Simpan user ke database
-    const user = { name, phone, email, password: hashedPassword };
-    db.query('INSERT INTO users SET ?', user, (err, result) => {
-      if (err) return res.status(500).json({ message: 'Database error.' });
-      res.status(201).json({ message: 'User registered successfully.' });
-    });
-  });
+  if (existedEmail) {
+    return res.status(400).json({ message: 'Email already registered.' });
+  }
+
+  // Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Simpan user ke database
+  await prisma.user.create({
+    data: {
+      name: name,
+      phone: phone,
+      email: email,
+      auth: hashedPassword
+    }
+  })
+
+  // if (err) return res.status(500).json({ message: 'Database error.' });
+
+  return res.status(201).json({ message: 'User registered successfully.' });
 });
 
 // Login User
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
     return res.status(400).json({ message: 'Email and password are required.' });
   }
 
-  db.query('SELECT * FROM users WHERE email = ?', [email], async (err, result) => {
-    if (err) return res.status(500).json({ message: 'Database error.' });
-    if (result.length === 0) {
-      return res.status(404).json({ message: 'User not found.' });
+  const user = await prisma.user.findUnique({
+    where: {
+      email
     }
+  })
 
-    const user = result[0];
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid password.' });
-    }
+  // if (err) return res.status(500).json({ message: 'Database error.' });
 
-    // Generate JWT
-    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.status(200).json({ message: 'Login successful.', token });
-  });
+  if (!user) {
+    return res.status(404).json({ message: 'User not found.' });
+  }
+
+  //check password
+  const isPasswordValid = await bcrypt.compare(password, user.auth);
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: 'Invalid password.' });
+  }
+
+  // Generate JWT
+  const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  res.status(200).json({ message: 'Login successful.', token });
 });
 
 module.exports = router;
